fde9ac5b00e0a3d33962948669930ef9c31af1e1
7Luk3e7 <lukewennerholm7@gmail.com>
2026-07-13 02:05:31 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,11 @@ local LOAD_DELAY = 3 -- Time (in seconds) allowed for game files and pets to loa
 
 -- Pets added here will only be sent to the SECONDARY webhook and will NOT trigger the main webhook
 local SECONDARY_LIST = {
-    "raccoon" -- Add more pets here using lowercase text, separated by commas (e.g., "fox", "cat")
+    "raccoon",
+    "unicron",
+    "goldendragonfly",
+    "firefly"
+    -- Add more pets here using lowercase text, separated by commas (e.g., "fox", "cat")
 }
 
 -- Services
